1c61759121c8a714406419569660ae54978da22a
Zahravaziri1382 <167520937+Zahravaziri1382@users.noreply.github.com>
2024-05-23 20:37:21 +0330

    Add files via upload

---
+++ cod_tas.ino
@@ -0,0 +1,28 @@
+#include "SevSeg.h"
+SevSeg sevseg;
+int buttonpin = 10;
+//int lastButtonstate = LOW;
+int currenButtonState;
+void setup() {
+  byte numDigits = 1;
+  byte digitPins[] = {};
+  byte segmentPins[] = {3, 2, 8, 7, 6, 5, 4, 9};
+  bool resistorsOnSegments = true;
+  bool updateWithDelays = true;
+  byte hardwareConfig = COMMON_ANODE; //
+  sevseg.begin(hardwareConfig, numDigits, digitPins , segmentPins, resistorsOnSegments);
+  pinMode(buttonpin, INPUT);
+  sevseg.setBrightness(90);
+}
+
+void loop() {
+  currenButtonState = digitalRead(buttonpin);
+  if (currenButtonState == HIGH)
+  {
+    int randomNumber = random(1, 7); // put your main code here, to run repeatedly:
+    sevseg.setNumber(randomNumber);
+    sevseg.refreshDisplay();
+    delay(500);
+  }
+//lastButtonstate = currenButtonState;
+}
